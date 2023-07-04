4b2c84df8203e6392ecf91f1d1d485fb94bed904
yk <17173as@163.com>
2023-07-04 10:23:17 +0800

    链表相关demo

---
+++ base/src/main/java/com/hz/yk/algo/camp/ch04_list/LC20valid_parentheses.java
@@ -0,0 +1,38 @@
+package com.hz.yk.algo.camp.ch04_list;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ * @author wuzheng.yk
+ * @date 2023/7/3
+ */
+public class LC20valid_parentheses {
+    static Map<Character, Character> cache = new HashMap<>();
+    static {
+        cache.put(')', '(');
+        cache.put(']', '[');
+        cache.put('}', '{');
+    }
+    
+    
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (cache.containsKey(c) ) {
+                if (stack.isEmpty() || stack.pop() != cache.get(c)) {
+                    return false;
+                }
+            }else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
