a56b818abc19aa59cf67e6f37a0d73c7c56316b0
yk <17173as@163.com>
2023-08-01 15:34:12 +0800

    数组和链表相关联系

---
+++ base/src/main/java/com/hz/yk/algo/camp/ch04_list/LC11container_with_most_water.java
@@ -0,0 +1,45 @@
+package com.hz.yk.algo.camp.ch04_list;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+ * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ * 返回容器可以储存的最大水量。
+ * https://leetcode.cn/problems/container-with-most-water/
+ *
+ * @author wuzheng.yk
+ * @date 2023/7/31
+ */
+public class LC11container_with_most_water {
+
+    public int maxArea(int[] height) {
+        int max = 0;
+        int left = 0, right = height.length - 1;
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            max = Math.max(max, h * (right - left));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            } 
+        }
+        return max;
+
+    }
+
+    @Test
+    public void test(){
+        LC11container_with_most_water test = new LC11container_with_most_water();
+        int[] result_49 = new int[]{ 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+        final int result = test.maxArea(result_49);
+        assertEquals(49, result);
+
+        final int[] result_1 = { 1, 1 };
+        assertEquals(1, test.maxArea(result_1));
+
+    }
+}
---
+++ base/src/main/java/com/hz/yk/algo/camp/ch04_list/ListNode.java
@@ -0,0 +1,49 @@
+package com.hz.yk.algo.camp.ch04_list;
+
+/**
+ * @author wuzheng.yk
+ * @date 2023/7/31
+ */
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+
+    public ListNode() {}
+
+    public ListNode(int val) {this.val = val;}
+
+    public ListNode(int... vals) {
+        if (vals.length < 1) {
+            return;
+        }
+        val = vals[0];
+        if (vals.length < 2) {
+            return;
+        }
+        next = new ListNode(vals[1]);
+
+        ListNode tmp = next;
+        for (int i = 2; i < vals.length; i++) {
+            tmp.next =  new ListNode(vals[i]);
+            tmp = tmp.next;
+        }
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+    
+    @Override
+    public String toString() {
+        String s = String.valueOf(val);
+        ListNode tmp = next;
+        while (tmp != null) {
+            s += "," +tmp.val;
+            tmp = tmp.next;
+        }
+        return s;
+    }
+
+}
