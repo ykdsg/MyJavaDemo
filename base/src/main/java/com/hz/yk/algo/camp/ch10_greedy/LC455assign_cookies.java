a1fdd9b0af57d3749465c7e926b424c3f03da15c
yk <17173as@163.com>
2023-08-09 16:54:29 +0800

    贪心算法相关练习

---
+++ base/src/main/java/com/hz/yk/algo/camp/ch10_greedy/LC455assign_cookies.java
@@ -0,0 +1,59 @@
+package com.hz.yk.algo.camp.ch10_greedy;
+
+import java.util.Arrays;
+
+/**
+ * 假设你是一位很棒的家长，想要给你的孩子们一些小饼干。但是，每个孩子最多只能给一块饼干。
+ * 对每个孩子 i，都有一个胃口值 g[i]，这是能让孩子们满足胃口的饼干的最小尺寸；并且每块饼干 j，都有一个尺寸 s[j] 。如果 s[j] >= g[i]，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。你的目标是尽可能满足越多数量的孩子，并输出这个最大数值。
+ * https://leetcode.cn/problems/assign-cookies/description/
+ *
+ * @author wuzheng.yk
+ * @date 2023/8/8
+ */
+public class LC455assign_cookies {
+
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count = 0;
+
+        for (int i = 0, j = 0; i < g.length && j < s.length; i++, j++) {
+
+            //如果饼干不能满足小朋友，寻找下一块
+            while (j < s.length && g[i] > s[j]) {
+                j++;
+            }
+            //说明存在满足小朋友的饼干
+            if (j < s.length) {
+                count++;
+            }
+
+        }
+
+        return count;
+
+    }
+
+    //更简洁的写法，从胃口大的开始，这样饼干的下标就不用每次移动
+    public int findContentChildren2(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count = 0;
+        //饼干下标
+        int index = s.length - 1;
+        for (int i = g.length - 1; i >= 0; i--) {
+            //如果饼干能满足胃口，注意index需要保证不越界
+            if (index >= 0 && s[index] >= g[i]) {
+                index--;
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        LC455assign_cookies test = new LC455assign_cookies();
+        final int result = test.findContentChildren2(new int[]{ 1, 3, 2 }, new int[]{ 1, 1 });
+        System.out.println(result);
+    }
+}
---
+++ base/src/main/java/com/hz/yk/algo/camp/ch10_greedy/LC874walking_robot_simulation.java
@@ -0,0 +1,58 @@
+package com.hz.yk.algo.camp.ch10_greedy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 机器人在一个无限大小的 XY 网格平面上行走，从点 (0, 0) 处开始出发，面向北方。该机器人可以接收以下三种类型的命令
+ * 返回机器人距离原点的 最大欧式距离 的 平方
+ * 1 <= commands.length <= 104
+ * commands[i] 的值可以取 -2、-1 或者是范围 [1, 9] 内的一个整数。
+ * https://leetcode.cn/problems/walking-robot-simulation/description/
+ * @author wuzheng.yk
+ * @date 2023/8/9
+ */
+public class LC874walking_robot_simulation {
+    //使用哈希表缓存障碍物
+    public int robotSim(int[] commands, int[][] obstacles) {
+        // 表示朝向北、东、南、西 方向行走一步的坐标变化
+        int[] direX = new int[]{ 0, 1, 0, -1 };
+        int[] direY = new int[]{ 1, 0, -1, 0 };
+        
+        Set<Integer> obstCache = new HashSet<>();
+        final double uplimit = Math.pow(10, 5);
+        final double lowlimit = Math.pow(-10, 5);
+        
+        for (int[] obstacle : obstacles) {
+            //根据上面command 的范围，就算全朝一个方向走，也不可能走出相应的范围大小，所以障碍物在这个范围外不用考虑
+            if (obstacle[0] > lowlimit && obstacle[0] < uplimit && obstacle[1] > lowlimit && obstacle[1]<uplimit) {
+                obstCache.add((int) (uplimit * obstacle[0] + obstacle[1]));
+            }
+        }
+        //刚开始面向北面
+        int curDire = 0;
+        int ans = 0;
+        int curX = 0, curY = 0;
+        for (int command : commands) {
+            if (command == -1) {  // -1：向右转 90 度
+                curDire = (curDire + 1) % 4;
+            } else if (command == -2) { //-2：向左转 90 度
+                curDire = (curDire + 3) % 4;
+            }else {  //  1 <= x <= 9：向前移动 x 个单位长度
+                for (int i = 0; i < command; i++) {
+                    int nx = curX + direX[curDire];
+                    int ny = curY + direY[curDire];
+                    //如果是障碍那就停止等待下一步指令
+                    if (obstCache.contains((int) (nx * uplimit + ny))) {
+                        break;
+                    }else {
+                        curX = nx;
+                        curY = ny;
+                        ans = Math.max(ans, curX * curX + curY * curY);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
