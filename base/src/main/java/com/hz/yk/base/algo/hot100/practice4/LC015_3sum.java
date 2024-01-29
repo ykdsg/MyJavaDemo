78b8faf7cde81c29449e779525a5ff8308a6c4fd
yk <17173as@163.com>
2024-01-29 11:41:20 +0800

    复习

---
+++ base/src/main/java/com/hz/yk/base/algo/hot100/practice4/LC015_3sum.java
@@ -0,0 +1,63 @@
+package com.hz.yk.base.algo.hot100.practice4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * https://leetcode.cn/problems/3sum/description/
+ * 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请
+ * 你返回所有和为 0 且不重复的三元组。
+ * 注意：答案中不可以包含重复的三元组。
+ *
+ * @author wuzheng.yk
+ * @date 2024/1/29
+ */
+public class LC015_3sum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        final int len = nums.length;
+        List<List<Integer>> result = new ArrayList<>();
+        if (len < 3) {
+            return result;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < len - 2; i++) {
+            if (nums[i] > 0) {
+                return result;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1, right = len - 1;
+            while (left < right) {
+                final int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left = getNextLeft(nums, left, right);
+                    right = getNextRight(nums, left, right);
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+
+    private int getNextRight(int[] nums, int left, int right) {
+        while (left < right && nums[right] == nums[right - 1]) {
+            right--;
+        }
+        return right - 1;
+    }
+
+    private int getNextLeft(int[] nums, int left, int right) {
+        while (left < right && nums[left] == nums[left + 1]) {
+            left++;
+        }
+        return left + 1;
+    }
+
+}
