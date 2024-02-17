f26bd7315874231f05111247545e9ec67dbd494b
yk <17173as@163.com>
2024-02-17 10:51:16 +0800

    复习

---
+++ base/src/main/java/com/hz/yk/base/algo/hot100/practice7/LC004median_of_two.java
@@ -0,0 +1,41 @@
+package com.hz.yk.base.algo.hot100.practice7;
+
+/**
+ * https://leetcode.cn/problems/median-of-two-sorted-arrays/description/
+ * 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+ *
+ * @author wuzheng.yk
+ * @date 2024/2/17
+ */
+public class LC004median_of_two {
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        final int len = nums1.length + nums2.length;
+        int k = len / 2 + 1;
+        if (len % 2 == 1) {
+            return getKth(nums1, nums2, 0, 0, k);
+        } else {
+            return (getKth(nums1, nums2, 0, 0, k) + getKth(nums1, nums2, 0, 0, k - 1)) / 2.0;
+        }
+    }
+
+    private int getKth(int[] nums1, int[] nums2, int index1, int index2, int k) {
+        if (index1 >= nums1.length) {
+            return nums2[index2 + k - 1];
+        }
+        if (index2 >= nums2.length) {
+            return nums1[index1 + k - 1];
+        }
+        if (k == 1) {
+            return Math.min(nums1[index1], nums2[index2]);
+        }
+        int half = k / 2;
+        int newIndex1 = Math.min(index1 + half, nums1.length) - 1;
+        int newIndex2 = Math.min(index2 + half, nums2.length) - 1;
+        if (nums1[newIndex1] <= nums2[newIndex2]) {
+            return getKth(nums1, nums2, newIndex1 + 1, index2, k - (newIndex1 - index1 + 1));
+        } else {
+            return getKth(nums1, nums2, index1, newIndex2 + 1, k - (newIndex2 - index2 + 1));
+        }
+    }
+}
