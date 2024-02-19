059297bcfd65b54aabc72eed043a8d086d62bcbd
yk <17173as@163.com>
2024-02-19 15:06:53 +0800

    复习算法题

---
+++ base/src/main/java/com/hz/yk/base/algo/hot100/practice5/LC002add_two_numbers.java
@@ -0,0 +1,35 @@
+package com.hz.yk.base.algo.hot100.practice5;
+
+import com.hz.yk.base.algo.camp.ch03_array.ListNode;
+
+/**
+ * https://leetcode.cn/problems/add-two-numbers/description/
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * @author wuzheng.yk
+ * @date 2024/2/19
+ */
+public class LC002add_two_numbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode();
+        ListNode cur = pre;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int v1 = l1 == null ? 0 : l1.val;
+            int v2 = l2 == null ? 0 : l2.val;
+            final int sum = v1 + v2 + carry;
+            cur.next = new ListNode(sum % 10);
+            carry = sum >= 10 ? 1 : 0;
+            cur = cur.next;
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+        }
+        if (carry > 0) {
+            cur.next = new ListNode(1);
+        }
+        return pre.next;
+    }
+}
